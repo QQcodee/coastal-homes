dce13f3bb93ced8374f0c0b29bd738c8935e80f6
QQcodee <castawebdesign@gmail.com>
2024-11-18 12:41:50 -0600

    Vercel deploy

---
+++ src/components/features.tsx
@@ -0,0 +1,180 @@
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import {
+  CloudDrizzle,
+  Construction,
+  HandCoins,
+  PencilRuler,
+  ShieldCheck,
+  UserRoundCog,
+  UsersRound,
+  Zap,
+} from "lucide-react";
+
+export function CarouselFeatures() {
+  return (
+    <Carousel opts={{ loop: true }} className="w-full max-w-sm sm:max-w-7xl ">
+      <CarouselContent className="-ml-1">
+        <CarouselItem className="pl-1 md:basis-1/2 lg:basis-1/3">
+          <div className="p-1">
+            <Card>
+              <CardContent className="flex aspect-square items-start justify-s p-6 bg-slate-100 rounded-xl hover:bg-slate-200">
+                <div className="flex flex-col gap-5 ">
+                  <CloudDrizzle className="h-[100px] w-[100px]" />
+                  <span className="text-2xl font-bold">Weather Resistance</span>
+                  <p>
+                    Modular homes are built indoors in controlled environments,
+                    shielding construction from rain, wind, and other elements.
+                    This ensures structural integrity and durability, making
+                    modular homes highly resilient in extreme weather
+                    conditions.
+                  </p>
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+        </CarouselItem>
+        <CarouselItem className="pl-1 md:basis-1/2 lg:basis-1/3">
+          <div className="p-1">
+            <Card>
+              <CardContent className="flex aspect-square items-start justify-s p-6 bg-slate-100 rounded-xl hover:bg-slate-200">
+                <div className="flex flex-col gap-5 ">
+                  <HandCoins className="h-[100px] w-[100px]" />
+                  <span className="text-2xl font-bold">Cost Efficiency </span>
+                  <p>
+                    Modular construction minimizes waste and labor costs through
+                    efficient factory production. This streamlined approach
+                    reduces expenses and often results in more predictable
+                    budgeting compared to traditional builds.
+                  </p>
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+        </CarouselItem>
+        <CarouselItem className="pl-1 md:basis-1/2 lg:basis-1/3">
+          <div className="p-1">
+            <Card>
+              <CardContent className="flex aspect-square items-start justify-s p-6 bg-slate-100 rounded-xl hover:bg-slate-200">
+                <div className="flex flex-col gap-5 ">
+                  <ShieldCheck className="h-[100px] w-[100px]" />
+                  <span className="text-2xl font-bold">Quality Control </span>
+                  <p>
+                    With standardized manufacturing processes, modular homes are
+                    built to strict quality standards. Inspections at each stage
+                    in the factory ensure high-quality materials, precise
+                    assembly, and consistent performance.
+                  </p>
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+        </CarouselItem>
+        <CarouselItem className="pl-1 md:basis-1/2 lg:basis-1/3">
+          <div className="p-1">
+            <Card>
+              <CardContent className="flex aspect-square items-start justify-s p-6 bg-slate-100 rounded-xl hover:bg-slate-200">
+                <div className="flex flex-col gap-5 ">
+                  <PencilRuler className="h-[100px] w-[100px]" />
+                  <span className="text-2xl font-bold">
+                    Customization & Options{" "}
+                  </span>
+                  <p>
+                    Modular construction offers flexibility with design,
+                    allowing buyers to personalize layouts, finishes, and
+                    features. Buyers can choose from various styles and
+                    configurations to create a home tailored to their needs.
+                  </p>
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+        </CarouselItem>
+        <CarouselItem className="pl-1 md:basis-1/2 lg:basis-1/3">
+          <div className="p-1">
+            <Card>
+              <CardContent className="flex aspect-square items-start justify-s p-6 bg-slate-100 rounded-xl hover:bg-slate-200">
+                <div className="flex flex-col gap-5 ">
+                  <UsersRound className="h-[100px] w-[100px]" />
+                  <span className="text-2xl font-bold">Community Impact </span>
+                  <p>
+                    Building modular homes reduces noise, pollution, and
+                    disruption in communities, as most of the construction
+                    occurs off-site. This method also often uses fewer
+                    resources, benefiting the environment and supporting
+                    sustainable building practices.
+                  </p>
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+        </CarouselItem>
+        <CarouselItem className="pl-1 md:basis-1/2 lg:basis-1/3">
+          <div className="p-1">
+            <Card>
+              <CardContent className="flex aspect-square items-start justify-s p-6 bg-slate-100 rounded-xl hover:bg-slate-200">
+                <div className="flex flex-col gap-5 ">
+                  <Construction className="h-[100px] w-[100px]" />
+                  <span className="text-2xl font-bold">Fast Construction </span>
+                  <p>
+                    Factory-built modular homes can be completed much faster
+                    than traditional homes. Indoor construction reduces delays
+                    due to weather, and streamlined assembly cuts overall build
+                    time, allowing owners to move in sooner.
+                  </p>
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+        </CarouselItem>
+        <CarouselItem className="pl-1 md:basis-1/2 lg:basis-1/3">
+          <div className="p-1">
+            <Card>
+              <CardContent className="flex aspect-square items-start justify-s p-6 bg-slate-100 rounded-xl hover:bg-slate-200">
+                <div className="flex flex-col gap-5 ">
+                  <UserRoundCog className="h-[100px] w-[100px]" />
+                  <span className="text-2xl font-bold">
+                    Precision Engineering{" "}
+                  </span>
+                  <p>
+                    Modular homes are constructed with precision engineering,
+                    meaning that each component is designed and manufactured
+                    with high accuracy. This results in tightly built homes with
+                    excellent energy efficiency and reduced maintenance needs.
+                  </p>
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+        </CarouselItem>
+        <CarouselItem className="pl-1 md:basis-1/2 lg:basis-1/3">
+          <div className="p-1">
+            <Card>
+              <CardContent className="flex aspect-square items-start justify-s p-6 bg-slate-100 rounded-xl hover:bg-slate-200">
+                <div className="flex flex-col gap-5 ">
+                  <Zap className="h-[100px] w-[100px]" />
+                  <span className="text-2xl font-bold">Zero Energy </span>
+                  <p>
+                    Many modular homes are designed with energy efficiency in
+                    mind, often integrating solar panels, energy-efficient
+                    appliances, and insulation to achieve zero-energy goals.
+                    This makes them highly sustainable, with minimal
+                    environmental impact and reduced utility costs.
+                  </p>
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+        </CarouselItem>
+      </CarouselContent>
+      <CarouselPrevious className="hidden md:block" />
+      <CarouselNext className="hidden md:block" />
+    </Carousel>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,30 @@
-import { BadgeCheck } from "lucide-react";
+import {
+  Activity,
+  AirVent,
+  BadgeCheck,
+  Flame,
+  House,
+  LayoutPanelTop,
+  Play,
+  PlayCircle,
+  ThermometerSnowflakeIcon,
+  ThermometerSun,
+  Wind,
+  WindIcon,
+} from "lucide-react";
 import { useState, useEffect, useRef } from "react";
+import {
+  ReactCompareSlider,
+  ReactCompareSliderImage,
+} from "react-compare-slider";
+import Logo from "../assets/LogoWithe.png";
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { CarouselFeatures } from "@/components/features";
 
 const Home = () => {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -33,24 +58,39 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="h-[max-content] w-screen bg-black flex flex-col   ">
+    <div className="h-[max-content] w-screen max-w-screen bg-black flex flex-col   ">
       <div
-        className={`h-[75px] w-full z-[11] flex items-center justify-between px-[75px] sticky top-0 transition-colors duration-300 ${
+        className={`h-[75px] hidden  w-full overflow-hidden max-w-screen z-[11] md:flex items-center justify-between px-[75px] sticky top-0 transition-colors duration-300 ${
           isScrolled ? "bg-[#001A24]" : "bg-transparent"
         }`}
       >
-        <p className="text-white text-[27px] font-bold ">
-          Complete Coastal Homes
-        </p>
+        <div className="flex items-center gap-3 cursor-pointer">
+          <img src={Logo} alt="" className="w-[100px] h-[100px]" />
+          <p className="text-white text-[27px] font-bold">
+            Florida Modular Coastal Homes
+          </p>
+        </div>
+
         <div className="flex items-center gap-10 ">
-          <p className="text-white font-bold ">How It Works</p>
-          <p className="text-white font-bold ">Financing</p>
-          <p className="text-white font-bold ">Gallery</p>
-          <p className="text-white font-bold ">Floorplans</p>
-          <p className="text-white font-bold ">Contact Us</p>
+          <p className="text-white font-bold cursor-pointer hover:text-gray-400 ">
+            How It Works
+          </p>
+          <p className="text-white font-bold cursor-pointer hover:text-gray-400 ">
+            Financing
+          </p>
+          <p className="text-white font-bold cursor-pointer hover:text-gray-400 ">
+            Gallery
+          </p>
+          <p className="text-white font-bold cursor-pointer hover:text-gray-400 ">
+            Floorplans
+          </p>
+          <p className="text-white font-bold cursor-pointer hover:text-gray-400 ">
+            Contact Us
+          </p>
         </div>
       </div>
-      <div className="h-[850px] w-screen overflow-hidden z-[1] mt-[-110px] ">
+
+      <div className="h-[850px] w-screen overflow-hidden z-[1] mt-[-110px] hidden lg:block ">
         <video autoPlay loop muted playsInline className="video-background">
           <source
             src="https://45669285.fs1.hubspotusercontent-na1.net/hubfs/45669285/Video%20Project%206.6.mp4"
@@ -60,29 +100,29 @@ const Home = () => {
         </video>
       </div>
 
-      <div className="h-[850px] w-screen mt-[-850px] py-[200px] px-[200px] z-10 flex flex-col bg-blue-500 bg-opacity-15 shadow-2xl">
+      <div className=" h-[850px] w-screen lg:mt-[-850px] py-[200px] px-[20px] lg:px-[200px] z-10 flex flex-col bg-blue-500 bg-opacity-15 shadow-2xl">
         <div className="flex items-center gap-2 ">
           <div className="h-[32px] bg-white w-[10px]"></div>
-          <p className="text-[36px] font-regular text-white overflow-hidden whitespace-nowrap animate-typewriter">
+          <p className="text-[19px] lg:text-[36px] font-regular text-white overflow-hidden sm:whitespace-nowrap animate-typewriter">
             The Best Hurricane-Proof Modular Construction Solution
           </p>
         </div>
-        <div className="flex items-center gap-2 mt-4">
-          <div className="w-1/2 ">
-            <p className="text-[64px] text-white font-bold font-poppins">
+        <div className=" items-center gap-2 mt-4">
+          <div className="w-full lg:w-1/2 ">
+            <p className="text-[36px] lg:text-[64px] text-white font-bold font-poppins mb-5">
               Build to Last
             </p>
-            <div className="h-[max-content] w-[600px] rounded-[12px] bg-white bg-opacity-10 backdrop-blur-md border border-white/20 shadow-lg py-[20px] px-[20px] flex flex-col gap-2">
+            <div className=" h-[max-content]  rounded-[12px] bg-white bg-opacity-10 backdrop-blur-md border border-white/20 shadow-lg py-[20px] px-[20px] flex flex-col gap-2">
               <p className="text-[21px] font-regular text-white font-poppins textShadow-lg ">
                 We are a team of professionals dedicated to providing you with
                 the highest quality Hurricane-Proof Modular Construction
                 Solution.
               </p>
-              <p className="text-[15px] font-regular text-blue-500 flex items-center gap-2 bg-white py-[10px] px-[20px] rounded-[20px] w-[max-content]  ">
+              <p className="text-[15px] font-regular text-blue-500 flex items-center gap-2 bg-white py-[10px] px-[20px] rounded-[20px] w-[max-content] mt-3 ">
                 Authorized Affinity Builder <BadgeCheck />
               </p>
             </div>
-            <p className="text-[19px] font-regular text-white mt-5 animate-fadeIn">
+            <p className="hidden lg:flex w-screen  text-[19px] lg:w-[600px] font-regular text-white mt-5 animate-fadeIn">
               We focus on building strong, durable, flood-resistant and
               energy-efficient homes in the Tampa Bay area with a focus on speed
               and quality. Our Homes are rated to withstand up to 180 MPH winds
@@ -90,14 +130,14 @@ const Home = () => {
               little as 6 months
             </p>
 
-            <button className="bg-blue-500 w-[max-content] text-white text-[21px] font-bold rounded-[20px] py-[10px] px-[20px] mt-[20px] hover:transform hover:scale-105 hover:bg-blue-600">
+            <button className=" bg-blue-500 w-[max-content] text-white text-[21px] font-bold rounded-[20px] py-[10px] px-[20px] mt-[20px] hover:transform hover:scale-105 hover:bg-blue-600">
               Learn More
             </button>
           </div>
         </div>
       </div>
 
-      <div className="animate-slideInRight absolute z-[10] h-[max-content] w-[700px] top-[300px] left-[1000px] rounded-[20px] bg-white/65 backdrop-blur-md border border-white/20 shadow-2xl py-[20px] px-[20px] flex flex-col gap-2 font-poppins">
+      <div className="hidden 2xl:flex 2xl:flex-col animate-slideInRight absolute z-[10] h-[max-content] w-[700px] top-[300px] left-[1000px] rounded-[20px] bg-white/65 backdrop-blur-md border border-white/20 shadow-2xl py-[20px] px-[20px] gap-2 font-poppins">
         <p className="text-center text-[21px] font-bold">
           Building Method Comparison
         </p>
@@ -233,18 +273,18 @@ const Home = () => {
         </button>
       </div>
 
-      <div className="h-[500px] w-full bg-white px-10 py-[80px]">
-        <div className="w-1/2 h-[max-content]">
+      <div className=" lg:h-[500px] w-full bg-white px-10 py-[80px]">
+        <div className="lg:w-1/2 h-full flex flex-col justify-center ">
           <div
             ref={ref}
-            className={`text-[32px] font-semibold text-white w-full h-[max-content] bg-blueB p-5 rounded-[8px] text-center  ${
+            className={`text-[21px] lg:text-[32px] font-semibold text-white w-full h-[max-content] bg-blueB p-3 rounded-[8px] text-center  ${
               isScrolled2 ? "animate-slideInLeft" : "opacity-0"
             }`}
           >
             Proven track record, highly scalable capacity
           </div>
           <p
-            className={` w-full p-5 text-[27px] text-justify ${
+            className={` w-full p-5 text-[21px] text-justify ${
               isScrolled2 ? "animate-fadeIn" : "opacity-0"
             }`}
           >
@@ -253,18 +293,103 @@ const Home = () => {
             factory-complete.{" "}
           </p>
           <div className="w-full flex justify-center ">
-            <button
-              className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${
-                isScrolled2 ? "animate-slideInRight" : "opacity-0"
-              }`}
-            >
-              Watch Real Delivery and Install
-            </button>
+            <Popover>
+              <PopoverTrigger
+                className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex gap-2 items-center ${
+                  isScrolled2 ? "animate-slideInRight" : "opacity-0"
+                }`}
+              >
+                <PlayCircle /> Watch Real Delivery and Install
+              </PopoverTrigger>
+              <PopoverContent className="w-[max-content]">
+                <iframe
+                  src="https://www.youtube.com/embed/ur1-mW-Itf4?si=lZ8MjitieNgtMRiV"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; fullscreen"
+                  referrerpolicy="strict-origin-when-cross-origin"
+                ></iframe>
+              </PopoverContent>
+            </Popover>
           </div>
         </div>
       </div>
-      <div className="h-[500px] w-full bg-white flex flex-col items-center ">
-        asd
+
+      <div className="flex flex-col w-full h-[max-content] p-10 flex justify-center items-center bg-white gap-5">
+        <h2 className="text-[27px] font-semibold text-center">
+          Why Modular Construction?
+        </h2>
+        <div className=" flex justify-center bg-white">
+          <CarouselFeatures />
+        </div>
+      </div>
+
+      <div className="hidden h-[1000px] w-full bg-blueB px-10 py-[80px] flex flex-col gap-5 items-center">
+        <h2 className="text-[46px] font-bold text-white">
+          How Resistant is Modular Coastal Home?
+        </h2>
+        <h2 className="text-[27px] font-semibold text-white">Before & After</h2>
+
+        <div className="flex gap-5 w-full justify-center">
+          <div className="w-1/6 h-full flex flex-col justify-center gap-10 ">
+            <p className="text-[22px] font-semibold text-white flex flex-col gap-5 border-2 border-white p-3 rounded-[8px] hover:bg-blue-500">
+              <WindIcon className="w-[50px] h-[50px] text-white" />
+              Wind Resistant up to 180 MPH
+            </p>
+            <p className="text-[22px] font-semibold text-white flex flex-col gap-5 border-2 border-white p-3 rounded-[8px] hover:bg-blue-500">
+              <Flame className="w-[50px] h-[50px] text-white" />
+              Fire-resistant
+            </p>
+            <p className="text-[22px] font-semibold text-white flex flex-col gap-5 border-2 border-white p-3 rounded-[8px] hover:bg-blue-500">
+              <Activity className="w-[50px] h-[50px] text-white" />
+              Earthquake resistant{" "}
+              <span className="text-[15px]">
+                (have withstood 8+ magnitude earthquakes)
+              </span>
+            </p>
+          </div>
+          <div className="w-1/2 h-full flex flex-col justify-center">
+            <ReactCompareSlider
+              className="cursor-pointer"
+              itemOne={
+                <ReactCompareSliderImage
+                  src="https://affinitymodular.com/hs-fs/hubfs/before-hurricane.jpg?width=1496&height=945&name=before-hurricane.jpg"
+                  alt="Image one"
+                />
+              }
+              itemTwo={
+                <ReactCompareSliderImage
+                  src="https://affinitymodular.com/hs-fs/hubfs/after-hurricane.jpg?width=1496&height=945&name=after-hurricane.jpg"
+                  alt="Image two"
+                />
+              }
+            />
+          </div>
+          <div className="w-1/6 h-full flex flex-col justify-center gap-5 ">
+            <p className="text-[22px] font-semibold text-white flex flex-col gap-5 border-2 border-white p-3 rounded-[8px] hover:bg-blue-500">
+              <ThermometerSun className="w-[50px] h-[50px] text-white" />
+              Even temperatures year-round
+            </p>
+            <p className="text-[22px] font-semibold text-white flex flex-col gap-5 border-2 border-white p-3 rounded-[8px] hover:bg-blue-500">
+              <LayoutPanelTop className="w-[50px] h-[50px] text-white" />
+              Ultra-high-performance windows
+            </p>
+            <p className="text-[22px] font-semibold text-white flex flex-col gap-5 border-2 border-white p-3 rounded-[8px] hover:bg-blue-500">
+              <AirVent className="w-[50px] h-[50px] text-white" />
+              Advanced fresh air systems and filtration{" "}
+              <span className="text-[15px]">
+                Our systems replace the air in your home 3+ times per day, and
+                remove particles like dust and pollen
+              </span>
+            </p>
+          </div>
+        </div>
+        <span className="text-[19px] font-light text-white">
+          Category 5 Hurricane Ian
+        </span>
+      </div>
+      <div className="h-[1000px] w-full bg-white px-10 py-[80px] flex flex-col gap-5 items-center">
+        <h2 className="text-[46px] font-bold text-black">Gallery </h2>
       </div>
     </div>
   );
